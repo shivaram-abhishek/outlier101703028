08867111273af77aef6de3f42317fa3744d8e79d
shivaram-abhishek <47098858+shivaram-abhishek@users.noreply.github.com>
2020-02-16 21:46:29 +0530

    Create setup.py

---
+++ outlier101703028/setup.py
@@ -0,0 +1,57 @@
+from distutils.core import setup
+
+setup(
+
+  name = 'outlier101703028',
+
+  packages = ['outlier101703028'],
+
+  version = '0.1',
+
+  license='MIT',
+
+  description = 'Outlier removal method',
+
+  author = 'Abhishek Shivaram',
+
+  author_email = 'ashivaram_be17@thapar.com',
+
+  url = 'https://github.com/shivaram-abhishek/outlier101703028',
+
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+
+  keywords = ['Outlier', 'Removal'],
+
+  install_requires=[
+
+          'numpy',
+
+          'pandas',
+          
+          'csv',
+          
+          'sys'
+
+      ],
+
+  classifiers=[
+
+    'Development Status :: 3 - Alpha',
+
+    'Intended Audience :: Developers',
+
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3',
+
+    'Programming Language :: Python :: 3.4',
+
+    'Programming Language :: Python :: 3.5',
+
+    'Programming Language :: Python :: 3.6',
+
+  ],
+
+)
